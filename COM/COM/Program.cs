4139c3d4c48da7f769baf502b8f05fad7fec112e
Roman Sapetin <saroman96@mail.ru>
2017-12-15 10:58:36 +0700

    asd2

--- COM/COM/Program.cs
+++ COM/COM/Program.cs
@@ -21,6 +21,7 @@ namespace COM
 			ModbusASCIIInterface com = new ModbusASCIIInterface();
 			double temp = -30;
 Где изменение?
+			ФФФФФФФФФФФФФФФФф
 			while (com.initPort("COM12"))
 			{
 				if (!com.setTargetTemperature(temp))
