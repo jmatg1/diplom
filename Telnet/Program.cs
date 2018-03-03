47d097f15427bbf51f23d0b6c5bce36861d462a0
Roman Sapetin <saroman96@mail.ru>
2018-03-03 16:29:38 +0700

    Добавил библиотеку nalib, есть только она и сам файл Programm. В библиотеке: отправка и прием, узнаем измерительые частоты

--- Telnet/Program.cs
+++ Telnet/Program.cs
@@ -3,102 +3,30 @@ using System.Text;
 using System.Net;
 using System.Net.Sockets;
 using static System.String;
+using System.Collections.Generic;
+using System.Linq;
 
-
-public class SynchronousSocketClient
+namespace Diplom
 {
-
-	public static void StartClient()
+	class Program
 	{
-		// Data buffer for incoming data.
-		byte[] bytes = new byte[256000];
-
-		// Connect to a remote device.
-		try
+		static void Main(string[] args)
 		{
-			// Establish the remote endpoint for the socket.
-			// This example uses port 11000 on the local computer.
-			IPHostEntry ipHostInfo = Dns.GetHostEntry("192.168.0.1");
-
-			IPAddress ipAddress = ipHostInfo.AddressList[0];
-			IPEndPoint remoteEP = new IPEndPoint(ipAddress, 23);
-
-
-			// Create a TCP/IP  socket.
-			Socket sender = new Socket(AddressFamily.InterNetwork,
-				SocketType.Stream, ProtocolType.Tcp);
-			//sender.SendTimeout = 10000;
-			sender.ReceiveTimeout = 10000; // ВРЕМЯ ОТВЕТА СЕРВЕРА
-			// Connect the socket to the remote endpoint. Catch any errors.
-			try
+			
+			//create a new telnet connection to hostname "gobelijn" on port "23"
+			nalib.TelnetConnection tc = new nalib.TelnetConnection("127.0.0.1", 9090);
+			Console.Write(tc.IsConnected);
+			List<double> freq = tc.GetFreq(1);
+			foreach (double combo in freq)
 			{
-				sender.Connect(remoteEP);
-
-				Console.WriteLine("Socket connected to {0}",
-					sender.RemoteEndPoint.ToString());
-
-				// Encode the data string into a byte array.
-				while (true)
-				{
-					
-					//Console.WriteLine("bytesSent " + bytesSent);
-					// Receive the response from the remote device.
-					int bytesRec = sender.Receive(bytes);
-
-					//Console.Write("bytesRec " + Convert.ToString(bytesRec));
-					if (IsNullOrEmpty(Convert.ToString(bytesRec)))
-					{
-						Console.WriteLine("NO OTVETA");
-					}
-
-
-					Console.WriteLine("{0}",Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-string n1 = Console.ReadLine();
-byte[] msg = Encoding.ASCII.GetBytes(n1 + "\n");
-
-// Отправляем. в инт кол-во символов
-int bytesSent = sender.Send(msg);
-				}
-				// Release the socket.
-				sender.Shutdown(SocketShutdown.Both);
-				sender.Close();
-
+				Console.WriteLine(combo);  
 			}
-			catch (ArgumentNullException ane)
-			{
-				Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-				sender.Shutdown(SocketShutdown.Both);
-				sender.Close();
-			}
-			catch (SocketException se)
-			{
-				Console.WriteLine("SocketException : {0}", se.ToString());
-				sender.Shutdown(SocketShutdown.Both);
-				sender.Close();
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Unexpected exception : {0}", e.ToString());
-				sender.Shutdown(SocketShutdown.Both);
-				sender.Close();
-			}
+
+			Console.ReadKey();
+		
+
+
 
 		}
-		catch (Exception e)
-		{
-			Console.WriteLine(e.ToString());
-		}
-	}
-	
-
-	public static int Main(String[] args)
-	{
-
-			StartClient();
-
-		//Console.ReadKey();
-
-		return 0;
 	}
 }
---
+++ Telnet/Properties/nalib.cs
@@ -0,0 +1,147 @@
+﻿// minimalistic telnet implementation
+// conceived by Tom Janssens on 2007/06/06  for codeproject
+//
+// http://www.corebvba.be
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Linq;
+
+namespace nalib
+{
+	enum Verbs
+	{
+		WILL = 251,
+		WONT = 252,
+		DO = 253,
+		DONT = 254,
+		IAC = 255
+	}
+
+	enum Options
+	{
+		SGA = 3
+	}
+
+	class TelnetConnection
+	{
+		TcpClient tcpSocket;
+
+		int TimeOutMs = 100;
+
+		public TelnetConnection(string Hostname, int Port)
+		{
+			tcpSocket = new TcpClient(Hostname, Port);
+
+		}
+		/// <summary>
+		/// Отправить команду
+		/// </summary>
+		public void WriteLine(string cmd)
+		{
+			Write(cmd + "\n");
+		}
+
+		private void Write(string cmd)
+		{
+			if (!tcpSocket.Connected) return;
+			byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd.Replace("\0xFF", "\0xFF\0xFF"));
+			tcpSocket.GetStream().Write(buf, 0, buf.Length);
+		}
+		/// <summary>
+		/// Получить ответ
+		/// </summary>
+		public string Read()
+		{
+			if (!tcpSocket.Connected) return null;
+			StringBuilder sb = new StringBuilder();
+			do
+			{
+				ParseTelnet(sb);
+				System.Threading.Thread.Sleep(TimeOutMs);
+			} while (tcpSocket.Available > 0);
+			return sb.ToString();
+		}
+		/// <summary>
+		/// Проверить соединение с сервером Телнет
+		/// </summary>
+		public bool IsConnected
+		{
+			get { return tcpSocket.Connected; }
+		}
+
+		void ParseTelnet(StringBuilder sb)
+		{
+			try
+			{
+				while (tcpSocket.Available > 0)
+				{
+					int input = tcpSocket.GetStream().ReadByte();
+					switch (input)
+					{
+						case -1:
+							break;
+						case (int)Verbs.IAC:
+							// interpret as command
+							int inputverb = tcpSocket.GetStream().ReadByte();
+							if (inputverb == -1) break;
+							switch (inputverb)
+							{
+								case (int)Verbs.IAC:
+									//literal IAC = 255 escaped, so append char 255 to string
+									sb.Append(inputverb);
+									break;
+								case (int)Verbs.DO:
+								case (int)Verbs.DONT:
+								case (int)Verbs.WILL:
+								case (int)Verbs.WONT:
+									// reply to all commands with "WONT", unless it is SGA (suppres go ahead)
+									int inputoption = tcpSocket.GetStream().ReadByte();
+									if (inputoption == -1) break;
+									tcpSocket.GetStream().WriteByte((byte)Verbs.IAC);
+									if (inputoption == (int)Options.SGA)
+										tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WILL : (byte)Verbs.DO);
+									else
+										tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WONT : (byte)Verbs.DONT);
+									tcpSocket.GetStream().WriteByte((byte)inputoption);
+									break;
+								default:
+									break;
+							}
+							break;
+						default:
+							sb.Append((char)input);
+							break;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("WAIT 1 MINUTE \r\n" + e);
+				System.Threading.Thread.Sleep(60000);
+			}
+		}
+
+		/// <summary>
+		/// Запрашиваем частоты, на которых измеряем
+		/// </summary>
+		public List<double> GetFreq(int channel)
+		{
+		    WriteLine(":SENS"+channel+":FREQ:DATA?");
+			string input = Read();
+			List<string> freq_string = input.Split(' ').ToList();
+			List<double> freq = freq_string.Select(x => double.Parse(x)).ToList();
+			/*foreach (double combo in freq)
+        {
+				Console.WriteLine(combo);
+        }*/
+
+
+			return freq;
+		}
+	}
+}
